a7f80249e85bc04d03884446298bf2b7130d7e35
root <dalareo@gmail.com>
2017-08-30 17:36:20 +0000

    modified url of apps

--- templates/content/index.php
+++ templates/content/index.php
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col-md-4">
       <div class="box">
-        <a href="https://blog.educaas.io"><img src="/custom_apps/educaasdashboard/img/wordpress.svg"></a>
+        <a href="https://educaas1.intechtenerife.es/index.php/apps/external/7"><img src="/custom_apps/educaasdashboard/img/wordpress.svg"></a>
         <h4 class="app-title">Blog</h4>
       </div>
     </div>
@@ -14,7 +14,7 @@
     </div>
     <div class="col-md-4">
       <div class="box">
-        <a href="https://chat.educaas.io"><img src="/custom_apps/educaasdashboard/img/rocket-chat.svg"></a>
+        <a href="https://educaas1.intechtenerife.es/index.php/apps/external/1"><img src="/custom_apps/educaasdashboard/img/rocket-chat.svg"></a>
         <h4 class="app-title">Chat</h4>
       </div>
     </div>
@@ -22,19 +22,19 @@
   <div class="row">
     <div class="col-md-4">
       <div class="box">
-        <a href="https://slides.educaas.io"><img src="/custom_apps/educaasdashboard/img/slides.jpg"></a>
+        <a href="https://educaas1.intechtenerife.es/index.php/apps/external/4"><img src="/custom_apps/educaasdashboard/img/slides.jpg"></a>
         <h4 class="app-title">Presentaciones</h4>
       </div>
     </div>
     <div class="col-md-4">
       <div class="box">
-        <a href="https://canvas.educaas.io"><img src="/custom_apps/educaasdashboard/img/canvas.svg"></a>
+        <a href="https://educaas1.intechtenerife.es/index.php/apps/external/2"><img src="/custom_apps/educaasdashboard/img/canvas.svg"></a>
         <h4 class="app-title">Creative Canvas</h4>
       </div>
     </div>
     <div class="col-md-4">
       <div class="box">
-        <a href="https://draw.educaas.io"><img src="/custom_apps/educaasdashboard/img/diagramas.jpg"></a>
+        <a href="https://draw.educaas1.intechtenerife.es"><img src="/custom_apps/educaasdashboard/img/diagramas.jpg"></a>
         <h4 class="app-title">Diagramas</h4>
       </div>
     </div>
@@ -42,7 +42,7 @@
   <div class="row">
     <div class="col-md-4">
       <div class="box">
-        <a href="https://agile.educaas.io"><img src="/custom_apps/educaasdashboard/img/wekan.svg"></a>
+        <a href="https://educaas1.intechtenerife.es/index.php/apps/external/5"><img src="/custom_apps/educaasdashboard/img/wekan.svg"></a>
         <h4 class="app-title">Gestión de proyectos</h4>
       </div>
     </div>
@@ -52,7 +52,7 @@
     </div>
     <div class="col-md-4">
       <div class="box">
-        <a href="https://git.educaas.io"><img src="/custom_apps/educaasdashboard/img/gogs.png"></a>
+        <a href="https://educaas1.intechtenerife.es/index.php/apps/external/6"><img src="/custom_apps/educaasdashboard/img/gogs.png"></a>
         <h4 class="app-title">Repositorio</h4>
       </div>
     </div>
--- templates/navigation/index.php
+++ templates/navigation/index.php
@@ -1,3 +1,3 @@
 <ul class="with-icon">
-	<li><a href="https://panel.educaas.io">EduCaaS Control panel</a></li>
+	<li><a href="https://educaas.intechtenerife.es">EduCaaS Control panel</a></li>
 </ul>
